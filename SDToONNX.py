fix to precision scope

SDToONNX.py
@@ -45,13 +45,13 @@ def Main():
     torchDevice = torch.device('cuda:0') #'cpu'
     
     dtype = torch.float32
-    autocast = False
+    autocast = True
 
     print('Using device:', torchDevice, flush=True)
 
     modelwrapper:modelWrap.ModelContext = CompVisSDModel.CompVisSDModel(dtype)
     modelwrapper.model_path = "E:/MLModels/stableDiffusion/sd-v1-4.ckpt" 
-    modelwrapper.modelName = "sd-v1-4-fp32-cuda" 
+    modelwrapper.modelName = "sd-v1-4-fp32-cuda-auto" 
     modelwrapper.ModelLoadSettings()
     modelwrapper.LoadModel(torchDevice)
 
@@ -83,9 +83,14 @@ def ConvertToONNXCuda(modelWrapper:modelWrap.ModelWrap, outFilePath:str, dtype, 
     #modelCtx.extra_args = {'cond': c, 'uncond': uc, 'cond_scale': condScale}
     modelCtx.kdiffModelWrap = denoisers.CFGDenoiser(modelWrapper.kdiffExternalModelWrap)
         
+    if str(device) == 'cpu':    
+        prec_dev = 'cpu'
+    else:
+        prec_dev = 'cuda'
+
     precision_scope = autocast if autocast_enable else nullcontext       
     with torch.no_grad():
-        with precision_scope(str(device)):
+        with precision_scope(prec_dev):
             torch.onnx.export(modelCtx.kdiffModelWrap,         # model being run 
                 (image_input, sigmaTensor, condTensor, uncondTensor, condscaleTensor),       # model input (or a tuple for multiple inputs)         
                 outFilePath,       # where to save the model  
@@ -141,9 +146,13 @@ def ConvertDecodeToONNXCuda(modelWrapper:modelWrap.ModelWrap, outFilePath:str, d
 
     dw = decodeWrap(modelWrapper.model)
 
+    if str(device) == 'cpu':    
+        prec_dev = 'cpu'
+    else:
+        prec_dev = 'cuda'
     precision_scope = autocast if autocast_enable else nullcontext         
     with torch.no_grad():
-        with precision_scope(str(device)):
+        with precision_scope(prec_dev):
             torch.onnx.export(dw,         # model being run 
                 (input_tensor),       # model input (or a tuple for multiple inputs)         
                 outFilePath,       # where to save the model  

src/onnxSampling.py
@@ -42,7 +42,7 @@ def sample_lms_ONNX(model:onnxruntime.InferenceSession, x, sigmas, extra_args=No
 @torch.no_grad()
 def sample_lms_ONNX_with_binding(model:onnxruntime.InferenceSession, x, sigmas, bindingType = torch.float32, extra_args=None, callback=None, disable=None, order=4):
 
-    condscaleTens = torch.FloatTensor([extra_args["cond_scale"]]).cuda()
+    #condscaleTens = torch.FloatTensor([extra_args["cond_scale"]]).cuda()
 
     extra_args = {} if extra_args is None else extra_args
     s_in = x.new_ones([x.shape[0]])
@@ -58,7 +58,7 @@ def sample_lms_ONNX_with_binding(model:onnxruntime.InferenceSession, x, sigmas, 
     binding.bind_output(name='modelOutput', device_type='cuda', device_id=0, element_type=bindingType, shape=tuple(denoised.shape), buffer_ptr=denoised.data_ptr())    
     binding.bind_input(name='uncond', device_type='cuda',device_id=0, element_type=bindingType, shape=tuple(extra_args["uncond"].shape), buffer_ptr=extra_args["uncond"].data_ptr())
     binding.bind_input(name='cond', device_type='cuda',device_id=0, element_type=bindingType, shape=tuple(extra_args["cond"].shape), buffer_ptr=extra_args["cond"].data_ptr())
-    binding.bind_input(name='cond_scale', device_type='cuda',device_id=0, element_type=bindingType, shape=tuple(condscaleTens.shape),buffer_ptr=condscaleTens.data_ptr())
+    binding.bind_input(name='cond_scale', device_type='cuda',device_id=0, element_type=bindingType, shape=tuple(extra_args["cond_scale"].shape),buffer_ptr=extra_args["cond_scale"].data_ptr())
 
     for i in trange(len(sigmas) - 1, disable=disable):
         q = (sigmas[i] * s_in).cuda() 